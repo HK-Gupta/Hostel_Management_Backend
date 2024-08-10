8ec8a2230a6bc4290eeac3122da1315165bcf481
Harsh Gupta <hkgupta1601@gmail.com>
2024-08-10 21:47:25 +0530

    Creating the email verification method

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -135,7 +135,9 @@ module.exports = {
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASSWORD,
